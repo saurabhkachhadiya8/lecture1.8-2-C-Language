d500108ce5440233f372aaeb4eef92c800c09d5b
theAkHilsarkar18 <akhilsodvadiya7@gmail.com>
2024-01-25 17:59:49 +0530

    first commit

---
+++ program8.2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	
+	int i,n;
+	
+	printf("Enter the starting point : ");
+	scanf("%d",&i);
+	printf("Enter the ending point : ");
+	scanf("%d",&n);
+	
+	while(i >= n){
+		
+		printf("%d\t",i);
+		
+		i--;
+		
+	}
+	
+}
---
+++ program8.4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	
+	int i,n,sum;
+	
+	printf("Enter the starting point : ");
+	scanf("%d",&i);
+	printf("Enter the ending point : ");
+	scanf("%d",&n);
+	
+	while(i <= n){
+		
+		printf("%d\t",i);
+		sum-=i;
+		i++;
+		
+		
+	}
+	printf(" = %d",sum);
+	
+}
